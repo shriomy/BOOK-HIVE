d53d32279dd113f7e99d6e986f2856f7547c3eb6
Thimothy <leitanthimothy@gmail.com>
2025-03-14 18:15:31 +0530

    deleted unnesasary files and refactored the code

--- backends/api/backend/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB", err);
-  });
