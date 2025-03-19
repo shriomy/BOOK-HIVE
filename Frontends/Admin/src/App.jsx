141b0096e9ab6bf52f230d8ac4d3fb392815637f
Hussainviwe <mhusmanhussain@gmail.com>
2025-03-19 22:43:20 +0530

    Add the donation and app

--- Frontends/Admin/src/App.jsx
+++ Frontends/Admin/src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { AdminContextProvider } from "./AdminContext"; // Change to AdminContext
 import AdminRoutes from "./routes/AdminRoutes"; // Change to AdminRoutes
 
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "http://localhost:3000";
 axios.defaults.withCredentials = true;
 
 function App() {
