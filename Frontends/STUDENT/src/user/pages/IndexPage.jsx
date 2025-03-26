13f2d81f239359baf330651eef8c5a1cf9ce4d11
Thimothy <leitanthimothy@gmail.com>
2025-03-26 10:58:19 +0530

    Updated IndexPage

--- Frontends/STUDENT/src/user/pages/IndexPage.jsx
+++ Frontends/STUDENT/src/user/pages/IndexPage.jsx
@@ -1,14 +1,33 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-
 import "../../index.css";
-// Import the stylesheet for this component
+import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
+import NumberAnimationBar from "../components/NumberAnimationBar";
+import { useLocation } from "react-router-dom";
+import ImageGrid from "../components/ImageGrid";
+import TeachersSection from "../components/TeachersSection";
 
-export default function IndexkPage() {
+export default function IndexPage() {
+  const location = useLocation();
+
+  // Use effect to scroll to top on route change
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#2c1f19] via-[#3e2723] to-[#000000] 30% h-screen overflow-hidden relative text-white">
-      index page
+    <div className="min-h-screen pb-5">
+      <HeroSection />
+      <AboutSection />
+
+      {/* Example Number Animation Bar */}
+      <div className="py-10">
+        <NumberAnimationBar start={0} end={1000} duration={2000} />
+      </div>
+      <ImageGrid />
+      <TeachersSection />
     </div>
   );
 }
