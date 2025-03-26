a27afcdb53333cf20ce530c82737f0c53df134e1
Thimothy <leitanthimothy@gmail.com>
2025-03-26 10:58:18 +0530

    Updated AboutUsPage

--- Frontends/STUDENT/src/user/pages/AboutUsPage.jsx
+++ Frontends/STUDENT/src/user/pages/AboutUsPage.jsx
@@ -2,12 +2,11 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-import "../../index.css";
 // Import the stylesheet for this component
 
 export default function AboutUsPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#2c1f19] via-[#3e2723] to-[#000000] 30% h-screen overflow-hidden relative text-white">
+    <div className="min-h-screen flex items-center justify-center h-screen overflow-hidden relative">
       about page
     </div>
   );
